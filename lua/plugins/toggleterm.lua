0ca4c7cad39081eb4b948796d664ec851b400764
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-04-09 17:15:02 +0200

    Made toggle always a floating terminal.
    Floating toggle terminal.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -30,7 +30,8 @@ return {
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
       terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
       persist_size = true,
-      direction = "horizontal" or "vertical" or "window" or "float",
+      direction = "float",
+      -- direction = "horizontal" or "vertical" or "window" or "float",
       close_on_exit = true, -- close the terminal window when the process exits
     },
   },
